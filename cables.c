fbb8cdbb03426fb54c0382f254d3348a49d93427
Ambiguousrelic <82973790+Ambiguousrelic@users.noreply.github.com>
2023-04-26 20:55:09 -0300

    Update cables.c

--- cables.c
+++ cables.c
@@ -1,3 +1,4 @@
+
 /*
 Yessica Fariña ci 5013777
 Ralf Adam ci 5023482
@@ -7,10 +8,24 @@ Pamela Franco ci 5346389
 #include <stdlib.h>
 #include <time.h>
 
+void cables();
+
 
 int main(){
 
-
+cables();
 
 return 0;
+}
+
+
+void cables(){
+	char nombres[10][20] = {"Agustin", "Beatriz", "Carlos", "Daniela", "Eugenio", "Fabiola", "Gustavo", "Hilda", "Ignacio", "Juan"};
+	int i,x,y;
+	
+	for(i=0;i<10;i++){
+		printf("\n %d %s",i+1, nombres[i]);
+	}
+	
+	
 }
