8f096260c47666b149ecafb4ab9f100e795bb5b8
pame-web <84988217+pame-web@users.noreply.github.com>
2023-04-26 20:19:16 -0400

    Update cables.c

--- cables.c
+++ cables.c
@@ -1,4 +1,3 @@
-
 /*
 Yessica Fariña ci 5013777
 Ralf Adam ci 5023482
@@ -9,23 +8,49 @@ Pamela Franco ci 5346389
 #include <time.h>
 
 void cables();
-
+void opciones();
+void cargarNombre();
 
 int main(){
-
-cables();
-
+	srand(time(NULL));
+	opciones();
 return 0;
 }
 
 
+void opciones(){
+	int opcion=0;
+	printf("\nPrograma de ordenamiento de cables por alumnos");
+	printf("\n-------------------------------------------------");
+ 	do{	
+  		printf("\nDesea ingresar los nombres de los 10 alumnos: 1-Si 2-No\nRespuesta:");
+  		scanf("%d",&opcion);
+  		getchar();
+  		switch (opcion) 
+		{
+    		case 1:cargarNombre();
+        		break;
+    		case 2:cables();
+    			break;
+    		default:printf("\nOperacion inexistente\nLas operaciones son entre 1 y 2\n");}
+	}while (opcion!=0);	
+}
+
 void cables(){
 	char nombres[10][20] = {"Agustin", "Beatriz", "Carlos", "Daniela", "Eugenio", "Fabiola", "Gustavo", "Hilda", "Ignacio", "Juan"};
 	int i,x,y;
-	
 	for(i=0;i<10;i++){
 		printf("\n %d %s",i+1, nombres[i]);
 	}
-	
-	
+}
+
+void cargarNombre(){
+	char nombre[10][20];
+	int i,x,y;
+	for(i=0;i<10;i++){
+		printf("\n%d.",i+1);
+  		scanf("%d",&nombre[i]);
+  		getchar();
+  		printf("\n%d.",i+1,nombre[i]);
+}
 }
